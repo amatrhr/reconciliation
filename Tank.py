fcb39f267d0d850ed96b4b3bbfe946005f128134
CA Laurin <cl14022@outlook.com>
2019-11-30 18:05:53 -0700

    fix pep8 format errors

--- Tank.py
+++ Tank.py
@@ -1,4 +1,6 @@
-from numpy import sqrt, array, append
+from numpy import sqrt, array
+
+
 class Tank:
     def __init__(self, height=0., inflow=0., valve_coef=1., outlet_area=1.):
         self.height = height
@@ -10,21 +12,21 @@ class Tank:
         self.outflow = self.valve_coef * sqrt(9.81 * self.height)
 
     def update_level(self):
-        self.outflow = self.valve_coef*sqrt(9.81*self.height)
-        self.height = height  + (1/self.outlet_area)*(self.inflow - self.outflow)
+        self.outflow = self.valve_coef * sqrt(9.81 * self.height)
+        self.height = height + (1 / self.outlet_area) * (self.inflow - self.outflow)
         self.time += 1
         self.history.append([self.time, self.inflow, self.outflow, self.height])
 
     def set_inflow(self, new_inflow):
         self.inflow = new_inflow
 
-    def measure_inflow(self, gross_error = False):
+    def measure_inflow(self, gross_error=False):
         return self.inflow + gross_error
 
-    def measure_outflow(self, gross_error = False):
+    def measure_outflow(self, gross_error=False):
         return self.outflow + gross_error
 
-    def measure_height(self, gross_error = False):
+    def measure_height(self, gross_error=False):
         return self.height + gross_error
 
     def measure_history(self, gross_in, gross_out, gross_height):
