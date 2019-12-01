c8375cd6b2631f8410508b2d5b63bd80e2a7def3
CA Laurin <cl14022@outlook.com>
2019-11-30 17:52:05 -0700

    add update_level function to Tank class

--- Tank.py
+++ Tank.py
@@ -1,4 +1,4 @@
-from numpy import sqrt, array
+from numpy import sqrt, array, append
 class Tank:
     def __init__(self, height=0., inflow=0., valve_coef=1., outlet_area=1.):
         self.height = height
@@ -6,9 +6,11 @@ class Tank:
         self.valve_coef = valve_coef
         self.outlet_area = outlet_area
         self.time = 0
+        self.history = array([self.time, self.height])
 
     def update_level(self):
         outflow = self.valve_coef*sqrt(9.81*self.height)
         self.height = height  + (1/self.outlet_area)*(self.inflow - outflow)
         self.time += 1
+        self.history.append([self.time, self.height])
 
